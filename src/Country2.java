c89f675d0088870ba4712813cbff00c88b832099
Workstation <artem.shubat@yandex.ru>
2022-03-04 14:37:58 +0500

    Initial commit

---
+++ src/Country2.java
@@ -0,0 +1,50 @@
+    public class Country2 {
+
+
+        private String human;
+        private int age;
+        private int sex;
+        private int name;
+        private int lastName;
+
+
+        public String getHuman() {
+            return human;
+        }
+
+        public void setHuman(String human) {
+            this.human = human;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getSex() {
+            return sex;
+        }
+
+        public void setSex(int sex) {
+            this.sex = sex;
+        }
+
+        public int getName() {
+            return name;
+        }
+
+        public void setName(int name) {
+            this.name = name;
+        }
+
+        public int getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(int lastName) {
+            this.lastName = lastName;
+        }
+    }
---
+++ src/main2.java
@@ -0,0 +1,22 @@
+public class main2 {
+
+    private String name;
+    private int price;
+
+    public main2(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){ return name;  }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public int getPrice(){
+        return price;
+    }
+
+}
